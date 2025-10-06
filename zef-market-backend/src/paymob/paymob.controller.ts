b6aaeb05270270b3587b08c41c2696f346f2b47d
unknown <mohamedelzefzafy@gmail.com>
2025-10-07 00:58:57 +0300

    fix paymob payment-14

--- zef-market-backend/src/paymob/paymob.controller.ts
+++ zef-market-backend/src/paymob/paymob.controller.ts
@@ -19,7 +19,7 @@
 
 
 
-import { Controller, Post, Get, Body, Query } from '@nestjs/common';
+import { Controller, Post, Get, Body, Query, Req } from '@nestjs/common';
 import { PaymobService } from './paymob.service';
 
 @Controller('api/v1/paymob')
@@ -42,14 +42,21 @@ export class PaymobController {
   }
 
   // === 2️⃣ Webhook لمعالجة الدفع بعد Paymob ===
-@Get('webhook')
-async handleWebhookGet(@Query() query: any) {
-  console.log('Paymob Webhook received (GET):', query);
+// @Get('webhook')
+// async handleWebhookGet(@Query() query: any) {
+//   console.log('Paymob Webhook received (GET):', query);
 
-  await this.paymobService.handleWebhook(query); // استعمل query بدل body
+//   await this.paymobService.handleWebhook(query); // استعمل query بدل body
 
-  return { received: true };
-}
+//   return { received: true };
+// }
+
+
+  @Get('webhook')
+  async handleWebhook(@Body() payload: any, @Req() req: any) {
+    return this.paymobService.handleWebhook(payload);
+  }
+
 
 
 
